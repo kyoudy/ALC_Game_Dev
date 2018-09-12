a8c7b5241423c18c56d9e8ba0d484ec1e702087b
Unknown <karsen.y188@stu.nebo.edu>
2018-09-12 09:57:01 -0600

    ghj

---
+++ 2D Game/Assets/scripts/CharMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //Player grounded vaiables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+
+    void FixedUpdate(){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //this makes the character jump
+        if (Input.GetKeyDown(KeyCode.Space) && grounded){
+            Jump();
+        }
+    }
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+}
