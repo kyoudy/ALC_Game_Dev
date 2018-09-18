6ae301e520b11e4ba4aa9462b60d0d021cd350bf
Unknown <karsen.y188@stu.nebo.edu>
2018-09-18 10:27:21 -0600

    added db jump and stop sliding

--- 2D Game/Assets/scripts/CharMove.cs
+++ 2D Game/Assets/scripts/CharMove.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharMove : MonoBehaviour
-{
+public class CharMove : MonoBehaviour{
+
     // Player Movement Variables
     public int MoveSpeed;
     public float JumpHeight;
+    private bool doubleJump;
 
     //Player grounded vaiables
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+
+    //Non-Stick Player
+    private float moveVelocity;
+
 
     // Use this for initialization
     void Start(){
@@ -33,17 +38,33 @@ public class CharMove : MonoBehaviour
             Jump();
         }
 
+        //Double jump code
+        if (grounded)
+            doubleJump = false;
+
+        if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+            Jump();
+            doubleJump = true;
+        }
+        //Non-Stick Player
+        moveVelocity = 0f; 
+
         // this code makes the character move from side to side using the A&D kays
         if (Input.GetKey (KeyCode.D)){
             GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+            moveVelocity = MoveSpeed;
         }
         if (Input.GetKey (KeyCode.A)){
             GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+            moveVelocity = -MoveSpeed;
         }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    
     }
-        public void Jump(){
+
+    public void Jump(){
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
     }
 }
