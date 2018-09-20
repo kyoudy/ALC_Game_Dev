bb0c0aeab3846b228740bc0d8e28c2f53326e52d
Unknown <karsen.y188@stu.nebo.edu>
2018-09-20 10:27:42 -0600

    added Point system

---
+++ 2D Game/Assets/scripts/DeathBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if (other.name == "PC")
+        {
+            Debug.Log("Player Enters Death Zone");
+            Destroy(other);
+        }
+    
+    }
+}
---
+++ 2D Game/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour{
+    public static int Score;
+
+    Text Scoretext;
+
+    // Usee this for initialization
+    void Start(){
+        Scoretext = GetComponent<Text>();
+
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Score < 0)
+            Score = 0;
+
+        Scoretext.text = " " + Score;
+    }
+
+    public static void AddPoints(int PointsToAdd){
+        Score += PointsToAdd;
+    }
+
+    public static void Reset(){
+        Score = 0
+    }
+
+}
