c1088afd5a6ccef2bc7951473e0872ca3d184d00
Unknown <karsen.y188@stu.nebo.edu>
2018-09-28 10:28:14 -0600

    Respawn, no stick,

--- 2D Game/Assets/scripts/EnemyPatrol.cs
+++ 2D Game/Assets/scripts/EnemyPatrol.cs
@@ -8,38 +8,37 @@ public class EnemyPatrol : MonoBehaviour{
     public float MoveSpeed;
     public bool MoveRight;
 
-    //Wall Check
+    // Wall Check
     public Transform WallCheck;
     public float WallCheckRadius;
     public LayerMask WhatIsWall;
     private bool HittingWall;
+
     // Edge Check
     private bool NotAtEdge;
     public Transform EdgeCheck;
 
 
     // Update is called once per frame
-    void Update() {
+    void Update(){
         NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
 
         HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
 
-        if (HittingWall || !NotAtEdge)
-        {
+        if (HittingWall || !NotAtEdge){
             MoveRight = !MoveRight;
         }
 
         if (MoveRight){
-            transform.localScale = new Vector3(-1f,1f,1f);
+            transform.localScale = new Vector3(-0.2f, 0.2f, 1f);
             GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-        else {
-            transform.localScale = new Vector3(1f,1f,1f);
-            GetComponent<Rigidbody>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        else{
+            transform.localScale = new Vector3(0.2f, 0.2f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
     }
 }
-
 
 
 
---
+++ 2D Game/Assets/scripts/LevelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+    private Rigidbody2D PC;
+
+    // Particles
+    public GameObject DeathParticles;
+    public GameObject RespawnParticles;
+
+    //Respawn Delay
+    public float RespawnDelay;
+
+
+    // Point Penalty on Death
+    public int PointPenaltyOnDeath;
+
+    // Store Gravity Value
+    private float GravityStore;
+   
+
+    // Use this for initialization
+	void Start () {
+        PC = FindObjectOfType<Rigidbody2D>();
+	}
+	
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPCCo");
+    }
+
+    public IEnumerator RespawnPCCo(){
+        //Generate Death Particle
+        Instantiate(DeathParticle, PC.transform.position, PC.transform.rotation);
+        //hide Player
+        PC.enabled = false;
+        PC.GetComponent<Renderer>().enabled = false;
+        //Gravity Reset
+        gravityStore = PC.GetComponent<Rigidbody2D>().gravityScale;
+        PC.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        PC.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //Point Penalty
+        ScoreManager.AddPoints(-PointPenaltyOnDeath);
+        //Debug Message
+        Debug.Log("Player Respawn");
+        //Respawn Delay
+        yield return new WaitForSeconds(respawnDelay);
+        //Gravity Restore
+        PC.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+        //Math Player transform position
+        RespawnPlayer.transform.position = currentCheckPoint.transform.position;
+
+    }
