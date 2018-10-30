d60e85c7427d5d9cb56308d13fcbc2a19d65cf31
Unknown <karsen.y188@stu.nebo.edu>
2018-10-30 10:24:42 -0600

    added Camera Follow

---
+++ 2D Game/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public CharMove Player;
+    public bool isFollowing;
+
+
+    //Camera position offset
+    public float xOffset;
+    public float yOffset;
+
+    // Use this for initialization
+	void Start () {
+        Player = FindObjectOfType<CharMove>();
+
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isFollowing){
+            transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+        }
+		
+	}
+}
